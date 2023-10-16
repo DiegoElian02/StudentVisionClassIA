d6d65b499e7df79270d1aaa9096d416db00a038b
Diego Elián Rodríguez Cantú <75437643+DiegoElian02@users.noreply.github.com>
2023-10-16 03:43:33 -0600

    checkbox updated with face_recog

--- webpage/paginas/Grupo_2.py
+++ webpage/paginas/Grupo_2.py
@@ -3,7 +3,6 @@ from deta import Deta
 
 st.title("Inteligencia artificial avanzada I")
 # st.set_page_config(layout="wide")
-# Verificar si la lista de alumnos ya existe en el estado de la sesión
 # if "lista_alumnos" not in st.session_state:
 #     st.session_state.lista_alumnos = ["Diego Rodriguez (21 años)", "Ana Cardenas (21 años)", "Jose Romo (22 años)"]
 
@@ -28,12 +27,10 @@ def update_alumno(key, updates):
 def delete_alumno(key):
     return db.delete(key)
 
-# Diseño de dos columnas
 col1, col2 = st.columns([2, 1])
 
 with col1:
     st.write("Estos son los alumnos del grupo 2 de la clase de inteligencia artificial avanzada:")
-    # Mostrar lista de alumnos
     f = fetch_alumnos()
     alumnos = [alumno['nombre'] for alumno in f] 
     for alumno in alumnos:
@@ -42,15 +39,12 @@ with col1:
 with col2:
     st.subheader("Agregar alumno")
     
-    # Campos de entrada
     matricula_nueva = st.text_input("Matricula del alumno")
     nombre_nuevo = st.text_input("Nombre del alumno")
     apellido_nuevo = st.text_input("Apellido del alumno")
     edad_nuevo = st.number_input("Edad del alumno", min_value=1, max_value=100, step=1)
     
-    # Botón para agregar alumno
     add_button = st.button('Registrar')
     if add_button and nombre_nuevo:
         nuevo_alumno(matricula_nueva, nombre_nuevo, apellido_nuevo, edad_nuevo, 0, 0)
-        # Limpiar campos después de agregar
         st.experimental_rerun()
--- webpage/paginas/Grupo_2_camara.py
+++ webpage/paginas/Grupo_2_camara.py
@@ -22,98 +22,99 @@ def fetch_alumnos():
     res = db.fetch()
     return res.items
 
-def get_alumno(key):
-    return db.get(key)
-
-def update_alumno(key, updates):
-    return db.update(key, updates)
-
-def delete_alumno(key):
-    return db.delete(key)
-
-
 col1, col2, col3 = st.columns([3,1,1])
 
-with col1:
-    st.write("Cámara en tiempo real:")
-            
-    run = st.checkbox('Run Webcam')
-    FRAME_WINDOW = st.empty()
-    camera = cv2.VideoCapture(0)
-    while run:
-        _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(frame)
-    else:
-        st.write('Stopped')
 
+import os
 
-    import os
+def list_files_in_folder(folder_path):
+    file_names = []
+    for file in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, file)):
+            file_names.append(file)
+    return file_names
 
-    def list_files_in_folder(folder_path):
-        file_names = []
-        for file in os.listdir(folder_path):
-            if os.path.isfile(os.path.join(folder_path, file)):
-                file_names.append(file)
-        return file_names
+known_face_encodings = []
+known_face_names = []
 
-    known_face_encodings = []
-    known_face_names = []
+for person in list_files_in_folder('known_faces'):
+    face = face_recognition.load_image_file(f"known_faces/{person}")
+    face_face_encoding = face_recognition.face_encodings(face)[0]
+        
+    known_face_encodings.append(face_face_encoding)
+    known_face_names.append(person[:-4])
 
-    for person in list_files_in_folder('known_faces'):
-        face = face_recognition.load_image_file(f"known_faces/{person}")
-        face_face_encoding = face_recognition.face_encodings(face)[0]
-            
-        known_face_encodings.append(face_face_encoding)
-        known_face_names.append(person[:-4])
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+iter = 0
+reconocidos = set()
 
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    process_this_frame = True
-    iter = 0
-
-    # st.write("Cámara en tiempo real:")
-    # run = st.checkbox('Run Webcam')
-    # FRAME_WINDOW = st.empty()
-    # camera = cv2.VideoCapture(0)
-    # if run:
-    #     ret, frame = camera.read()
-    #     if process_this_frame:
-    #         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    #         rgb_small_frame = small_frame
-    #         face_locations = face_recognition.face_locations(rgb_small_frame)
-    #         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-    #         face_names = []
-    #         for face_encoding in face_encodings:
-    #             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    #             name = "Unknown"
-    #             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-    #             best_match_index = np.argmin(face_distances)
-    #             if matches[best_match_index]:
-    #                 name = known_face_names[best_match_index]
-    #             face_names.append(name)
-
-    #         process_this_frame = iter == 0
-    #         iter = (iter + 1) % 4
-
-    #         for (top, right, bottom, left), name in zip(face_locations, face_names):
-    #             top *= 4
-    #             right *= 4
-    #             bottom *= 4
-    #             left *= 4
-    #             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-    #             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-    #             font = cv2.FONT_HERSHEY_DUPLEX
-    #             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-    #         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #         FRAME_WINDOW.image(frame)
 
 st.write("Estos son los alumnos del grupo 2 de la clase de inteligencia artificial avanzada:")
 f = fetch_alumnos()
 alumnos = [alumno['nombre'] for alumno in f]
 asistencia = {alumno: {"asistio": False, "fecha": None, "participaciones": 0} for alumno in alumnos}
+
+for alumno in reconocidos:
+    print(alumno)
+    print(asistencia)
+    print("lol?")
+    if alumno in asistencia:
+        st.session_state[alumno] = True
+
+with col1:
+    # st.write("Cámara en tiempo real:")
+            
+    # run = st.checkbox('Run Webcam')
+    # FRAME_WINDOW = st.empty()
+    # camera = cv2.VideoCapture(0)
+    # while run:
+    #     _, frame = camera.read()
+    #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #     FRAME_WINDOW.image(frame)
+    # else:
+    #     st.write('Stopped')
+
+    st.write("Cámara en tiempo real con deteccion:")
+    run = st.checkbox('Run Webcam')
+    FRAME_WINDOW = st.empty()
+    camera = cv2.VideoCapture(0)
+    
+    while run:
+        _, frame = camera.read()
+        if process_this_frame:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+                face_names.append(name) 
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                for name in face_names:
+                    if name!= "Unknown" and name in st.session_state:
+                        st.session_state[name] = True
+        # process_this_frame = iter == 0
+        # iter = (iter + 1) % 2
+        process_this_frame = True
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
 
 def registo_dia(key: str, estado, fecha, participaciones):
     return db2.put({"alumno" : key, "asistio" : estado, "fecha" : fecha, "participaciones": participaciones})
@@ -121,10 +122,9 @@ def registo_dia(key: str, estado, fecha, participaciones):
 
 with col2:
     st.header("Registro de Asistencia")
-    
     for alumno in alumnos:
-        asistio = st.checkbox(f"Asistió {alumno}", asistencia[alumno]["asistio"])
-        asistencia[alumno]["asistio"] = asistio
+        asistio = st.checkbox(f"Asistió {alumno}", value=st.session_state[alumno])
+        st.session_state[alumno] = asistio
         if asistio:
             asistencia[alumno]["fecha"] = datetime.now()
             
