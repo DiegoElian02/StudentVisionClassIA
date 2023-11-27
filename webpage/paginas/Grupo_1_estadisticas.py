53615eebe1f2273faaddabbd9e30d0cd24c2fe3e
Diego Elián Rodríguez Cantú <75437643+DiegoElian02@users.noreply.github.com>
2023-11-26 20:46:00 -0600

    max

--- webpage/paginas/Grupo_1_estadisticas.py
+++ webpage/paginas/Grupo_1_estadisticas.py
@@ -38,7 +38,7 @@ data = [{**entry, 'day_of_week': calendar.day_name[datetime.strptime(entry['fech
 
 df_asistencias = pd.DataFrame(data)
 df_asistencias["fecha"] = df_asistencias["fecha"].apply(lambda x: pd.to_datetime(x).date())
-alumnos_hoy = df_asistencias[df_asistencias["fecha"] == datetime.now().date()]["alumno"].unique()
+alumnos_hoy = df_asistencias[df_asistencias["fecha"] == df_asistencias["fecha"].max()]["alumno"].unique()
 st.write(alumnos_hoy)
 alumnos = list(set([item['alumno'] for item in data]))
 
